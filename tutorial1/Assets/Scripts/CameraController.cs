f9595677837839bd6a6780012f6e7cb03537f42f
rachelbeeman <36116145+rachelbeeman@users.noreply.github.com>
2018-02-03 17:02:09 -0600

    The completed Roll-a-Ball tutorial
    This introductory tutorial involved creating a mini game where the player controls a rolling ball that picks up rotating cubes. A count is displayed of the number collected as well as win text when all cubes have been collected.

---
+++ tutorial1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Guranteed to run after all items have been processed. (We know absolutely that the player has already moved this frame.)
+	void LateUpdate () {
+
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ tutorial1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text CountText;
+    public Text WinText;
+
+    private Rigidbody rb;
+    private int count;
+
+
+    void Start() {
+
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        WinText.text = "";
+    }
+
+    // Update is called once per frame (before rendering the frame)
+    void Update () {
+		
+	}
+
+    // FixedUpdate is called before performing any physics calculations
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+        
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            count++;
+            if (count>=12)
+            {
+                WinText.text = "You win!";
+            }
+
+            SetCountText();
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    void SetCountText()
+    {
+        CountText.text = "Count: " + count.ToString();
+    }
+}
