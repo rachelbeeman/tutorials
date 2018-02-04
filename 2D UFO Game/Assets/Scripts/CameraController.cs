0cc48c62fc6990e1b66de700fcb63522322a3a07
rachelbeeman <36116145+rachelbeeman@users.noreply.github.com>
2018-02-03 20:37:08 -0600

    The 2D UFO Game Unity tutorial
    This tutorial is the 2D version of Roll-a-ball, complete with a moving player, sprite collision detection, and rotating sprite collection.

---
+++ 2D UFO Game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame--Late:after player has moved
+	void LateUpdate () {
+
+        transform.position = player.transform.position + offset;
+		
+	}
+}
---
+++ 2D UFO Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float Speed;
+    public Text CountText;
+    public Text WinText;
+
+    private Rigidbody2D rb2d;
+    private int count;
+
+    // Use this for initialization
+	void Start () {
+
+        rb2d = GetComponent<Rigidbody2D>();
+        Speed = 10;
+        count = 0;
+        SetCountText();
+        WinText.text = "";
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb2d.AddForce(movement * Speed);
+
+    }
+
+   void OnTriggerEnter2D(Collider2D other)
+   {
+        if (other.gameObject.CompareTag ("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+            if (count >= 12)
+            {
+                WinText.text = "You Win!";
+            }
+        }
+   }
+
+   void SetCountText()
+    {
+        CountText.text = "Count: " + count.ToString();
+    }
+}
